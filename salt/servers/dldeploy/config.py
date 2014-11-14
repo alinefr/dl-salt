dda7f36fb70b9eadcc4c16898920e16c376865bf
Aline Freitas <a.freitas@doubleleft.com>
2014-11-14 20:21:25 -0200

    fix mistype

--- salt/servers/dldeploy/config.py
+++ salt/servers/dldeploy/config.py
@@ -4,7 +4,7 @@ def run():
     config = {}
 
     SERVERS = []
-    for host, hostinfo in __['mine.get']('*', 'network.interfaces').items():
+    for host, hostinfo in __salt__['mine.get']('*', 'network.interfaces').items():
         SERVERS.append({"hostname": host,
                         "username": __salt__['pillar.get']('monit:http_access:user'),
                         "password": __salt__['pillar.get']('monit:http_access:password')})
